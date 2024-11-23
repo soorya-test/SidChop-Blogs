43b23df096a6858339b2e741c9c86d9c0d4b904c
Soorya U <sooryau7@gmail.com>
2024-11-23 20:09:08 +0530

    chore: Changed the Template Names
    Signed-off-by: Soorya U <sooryau7@gmail.com>

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
       <header className="px-4 lg:px-6 h-14 flex items-center">
         <Link className="flex items-center justify-center" href="#">
           <Mail className="h-6 w-6" />
-          <span className="sr-only">Acme Blog</span>
+          <span className="sr-only">SidChop Blog</span>
         </Link>
         <nav className="ml-auto flex gap-4 sm:gap-6">
           <Link
@@ -46,12 +46,12 @@ export default function Home() {
         </nav>
       </header>
       <main className="flex-1">
-        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48">
+        <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 xl:h-screen">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center space-y-4 text-center">
               <div className="space-y-2">
                 <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-                  Welcome to Acme Blog
+                  Welcome to SidChop Blog
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
                   Discover insightful articles, expert opinions, and the latest
@@ -96,7 +96,7 @@ export default function Home() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">
-          © 2023 Acme Blog. All rights reserved.
+          © 2023 SidChop Blog. All rights reserved.
         </p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
           <Link className="text-xs hover:underline underline-offset-4" href="#">
